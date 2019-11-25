b36ea906a5eded4cd96baa03e417f0ceb52d5c52
tanguiano92 <47006589+tanguiano92@users.noreply.github.com>
2019-11-25 12:51:18 -0800

    update

--- sketch.js
+++ sketch.js
@@ -3,7 +3,7 @@
 
 var frog;
 
-var grid_size = 50;
+var grid_size = 25;
 
 var rows = [];
 
@@ -22,24 +22,25 @@ function setup() {
   rows = [
     new Row(0, 1, 0, width, 0, 100, true),
     new Row(grid_size, 1, 0, width, 0, 0, true),
-    new Row(2 * grid_size, 2, 0.5, 4 * grid_size, 600, 10, true),
-    new Row(3 * grid_size, 3, -1.3, 2 * grid_size, 400, 30, true),
-    new Row(4 * grid_size, 2, 2.3, 3 * grid_size, 350, 25, true),
+    new Row(2 * grid_size, 2, 5, 4 * grid_size, 600, 10, true),
+    new Row(3 * grid_size, 3, 8, 2 * grid_size, 400, 30, true),
+    new Row(4 * grid_size, 2, 6, 3 * grid_size, 350, 25, true),
     new Row(5 * grid_size, 1, 0, width, 0, 0, true),
-    new Row(6 * grid_size, 3, 1.2, 1 * grid_size, 250, 100, false),
-    new Row(7 * grid_size, 2, -3.5, 1 * grid_size, 300, 150, false),
+    new Row(6 * grid_size, 3, 8, 1 * grid_size, 250, 100, false),
+    new Row(7 * grid_size, 2, 3, 1 * grid_size, 300, 150, false),
     new Row(8 * grid_size, 2, 2, 2 * grid_size, 400, 0, false),
     new Row(9 * grid_size, 2, 0, width, 0, 0, true),
     new Row(10, 1, 0, width, 0, 100, true),
     new Row(grid_size, 1, 0, width, 0, 0, true),
+    new Row(11 * grid_size, 2, 3, 4 * grid_size, 250, 0, true),
     new Row(12 * grid_size, 2, 0.5, 4 * grid_size, 600, 10, true),
-    new Row(13 * grid_size, 3, -1.3, 2 * grid_size, 400, 30, true),
-    new Row(14 * grid_size, 2, 2.3, 3 * grid_size, 350, 25, true),
+    new Row(13 * grid_size, 3, 3, 2 * grid_size, 400, 30, true),
+    new Row(14 * grid_size, 2, 6, 3 * grid_size, 350, 25, true),
     new Row(15 * grid_size, 1, 0, width, 0, 0, true),
-    new Row(16 * grid_size, 3, 1.2, 1 * grid_size, 250, 100, false),
-    new Row(17 * grid_size, 2, -3.5, 1 * grid_size, 300, 150, false),
-    new Row(18 * grid_size, 2, 2, 2 * grid_size, 400, 0, false),
-    new Row(99 * grid_size, 2, 0, width, 0, 0, true),
+    new Row(16 * grid_size, 3, 3, 1 * grid_size, 250, 100, false),
+    new Row(17 * grid_size, 2, 6, 1 * grid_size, 300, 150, false),
+    new Row(18 * grid_size, 4, 5, 2 * grid_size, 400, 0, false),
+    new Row(19 * grid_size, 3, 0, width, 0, 0, true),
   ];
 
   // if createCanvas
